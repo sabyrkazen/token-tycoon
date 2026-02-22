ee134a5c1360160499d171eb7d057e29e181ea62
Sabyrka <sabyrkazen@gmail.com>
2026-02-23 02:19:52 +0500

    feat: calculate cumulative level scores in score management for enhanced gameplay experience

--- src/stores/score.js
+++ src/stores/score.js
@@ -6,7 +6,21 @@ const levels = new Array(15)
   .fill(0)
   .map((_, i) => baseLevelScore * Math.pow(2, i))
 
-console.log(levels)
+const levelScores = levels.map((_, level) => {
+  let sum = 0
+
+  for (let [index, value] of levels.entries()) {
+    if (index >= level) {
+      return sum + value
+    }
+
+    sum += value
+  }
+
+  return sum
+})
+
+console.log(levelScores)
 
 export const useScoreStore = defineStore('score', {
   state: () => ({
