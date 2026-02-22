cb5a282aba574b02b482690927d930f9191d2508
Sabyrka <sabyrkazen@gmail.com>
2026-02-23 02:29:07 +0500

    refactor: simplify cumulative score calculation in score management for improved performance

--- src/stores/score.js
+++ src/stores/score.js
@@ -6,17 +6,10 @@ const levels = new Array(15)
   .fill(0)
   .map((_, i) => baseLevelScore * Math.pow(2, i))
 
-const levelScores = levels.map((_, level) => {
-  let sum = 0
+let sum = 0
 
-  for (let [index, value] of levels.entries()) {
-    if (index >= level) {
-      return sum + value
-    }
-
-    sum += value
-  }
-
+const levelScores = levels.map((value) => {
+  sum += value
   return sum
 })
 
